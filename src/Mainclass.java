d2c0c3e234d70fe36e67813de1363671ba800c33
pinha <pinhaeguia@gmail.com>
2022-03-15 09:37:20 +0100

    Cambio 3

--- src/Mainclass.java
+++ src/Mainclass.java
@@ -2,6 +2,6 @@ public class Mainclass {
     public static void main(String[] args) {
         System.out.println("MOMO");
         System.out.println("Commit 1");
-        System.out.println("2");
+        System.out.println("3");
     }
 }
